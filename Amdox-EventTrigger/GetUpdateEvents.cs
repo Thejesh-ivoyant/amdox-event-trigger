9df71d59bae5f9077c1f4f66f75680813d4d24f4
Thejesh-ivoyant <127406875+Thejesh-ivoyant@users.noreply.github.com>
2024-03-10 23:41:39 +0530

    updated class

--- Amdox-EventTrigger/GetUpdateEvents.cs
+++ Amdox-EventTrigger/GetUpdateEvents.cs
@@ -1,44 +1,83 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 using Azure.Messaging.EventHubs;
+using Microsoft.Azure.Amqp.Framing;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Amdox_EventTrigger
 {
+   
     public static class GetUpdateEvents
     {
+        static string url = "https://localhost:7042/uploadUserData";
+
         [FunctionName("GetUpdateEvents")]
         public static async Task Run([EventHubTrigger("amdox-eventhub", Connection = "amdox-events-connection-setting")] EventData[] events, ILogger log)
         {
             var exceptions = new List<Exception>();
+            var userList = new List<User>();
 
             foreach (EventData eventData in events)
             {
                 try
                 {
-                    // Replace these two lines with your processing logic.
-                    log.LogInformation($"C# Event Hub trigger function processed a message: {eventData.EventBody}");
-                    await Task.Yield();
+                    log.LogInformation($"BODY IS {Encoding.UTF8.GetString(eventData.Body.ToArray())}");
+
+                    string jsonString = Encoding.UTF8.GetString(eventData.Body.ToArray());
+
+                    // Try deserializing as an array of Users first
+                    var users = JsonConvert.DeserializeObject<User>(jsonString);
+                   
+                    
+                    userList.Add(users);
                 }
                 catch (Exception e)
                 {
-                    // We need to keep processing the rest of the batch - capture this exception and continue.
-                    // Also, consider capturing details of the message that failed processing so it can be processed again later.
+                    exceptions.Add(e);
+                    log.LogError($"Error deserializing JSON data: {e.Message}");
+                }
+            }
+
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+                    var json = JsonConvert.SerializeObject(userList);
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    var response = await httpClient.PostAsync(url, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("JSON data uploaded successfully.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error uploading JSON data. Status code: {response.StatusCode}");
+                    }
+                }
+                catch (Exception e)
+                {
                     exceptions.Add(e);
                 }
             }
 
-            // Once processing of the batch is complete, if any messages in the batch failed processing throw an exception so that there is a record of the failure.
-
             if (exceptions.Count > 1)
+            {
                 throw new AggregateException(exceptions);
+            }
 
             if (exceptions.Count == 1)
+            {
                 throw exceptions.Single();
+            }
         }
     }
 }
---
+++ Amdox-EventTrigger/user.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amdox_EventTrigger
+{
+    public class User
+    {
+        public string UserGuid { get; set; }
+        public string B2BUserld { get; set; }
+        public string B2BAccessCode { get; set; }
+        public string UttUID { get; set; }
+        public string Type { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public bool IsSynced { get; set; }
+        public string SyncStatus { get; set; }
+        public string ModificationDate { get; set; }
+        public string ModificationBatch { get; set; }
+        public string SyncDate { get; set; }
+    }
+}
