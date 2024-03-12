5b4519f3f4a1914fb5a2779b7b18d3416b4eb459
Thejesh-ivoyant <127406875+Thejesh-ivoyant@users.noreply.github.com>
2024-03-12 15:33:42 +0530

    orchestrated

--- Amdox-EventTrigger/GetUpdateEvents.cs
+++ Amdox-EventTrigger/GetUpdateEvents.cs
@@ -2,20 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 using Azure.Messaging.EventHubs;
 using Azure.Messaging.EventHubs.Producer;
-using Microsoft.Azure.Amqp.Framing;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace Amdox_EventTrigger
 {
-   
-    public  class GetUpdateEvents
+    public class GetUpdateEvents
     {
         static string url = "https://localhost:7042/uploadUserORToAR";
 
@@ -33,29 +30,21 @@ namespace Amdox_EventTrigger
 
                 foreach (var kvp in keyValuePairs)
                 {
-                   string jsonKeyValuePair = JsonConvert.SerializeObject(kvp);
-
-                    // Convert the JSON string to bytes
+                    string jsonKeyValuePair = JsonConvert.SerializeObject(kvp);
                     byte[] eventDataBytes = Encoding.UTF8.GetBytes(jsonKeyValuePair);
-
-                    // Create EventData with binary data
                     Azure.Messaging.EventHubs.EventData eventData = new Azure.Messaging.EventHubs.EventData(eventDataBytes);
-
                     eventDataList.Add(eventData);
                 }
 
                 await producerClient.SendAsync(eventDataList);
-
-
-                Console.WriteLine($"Successfully sent {keyValuePairs.Count} events to Event Hub.");
+                Console.WriteLine($"Successfully sent Reply events to Event Hub.\n");
                 await producerClient.DisposeAsync();
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Error sending events: {ex.Message}");
+                Console.WriteLine($"Error sending events in Post event function: {ex.Message}");
                 // Log the error for further investigation and potential retries
             }
-           
         }
 
         [FunctionName("GetUpdateEvents")]
@@ -68,14 +57,8 @@ namespace Amdox_EventTrigger
             {
                 try
                 {
-                    log.LogInformation($"BODY IS {Encoding.UTF8.GetString(eventData.Body.ToArray())}");
-
                     string jsonString = Encoding.UTF8.GetString(eventData.Body.ToArray());
-
-                    // Try deserializing as an array of Users first
                     var users = JsonConvert.DeserializeObject<User>(jsonString);
-                   
-                    
                     userList.Add(users);
                 }
                 catch (Exception e)
@@ -89,25 +72,8 @@ namespace Amdox_EventTrigger
             {
                 try
                 {
-                    foreach (var user in userList)
-                    {
-                        var json = JsonConvert.SerializeObject(user);
-                        var content = new StringContent(json, Encoding.UTF8, "application/json");
-
-                        var response = await httpClient.PostAsync(url, content);
-
-                        if (response.IsSuccessStatusCode)
-                        {
-                            Console.WriteLine($"User data for {user.UserGuid} uploaded successfully.");
-                            var jsonResponse = await response.Content.ReadAsStringAsync();
-
-                            await PostEvent(jsonResponse);
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Error uploading user data for {user.UserGuid}. Status code: {response.StatusCode}");
-                        }
-                    }
+                    var tasks = userList.Select(user => ProcessUserAsync(httpClient, user, url)).ToList();
+                    await Task.WhenAll(tasks);
                 }
                 catch (Exception e)
                 {
@@ -125,5 +91,32 @@ namespace Amdox_EventTrigger
                 throw exceptions.Single();
             }
         }
+
+        public static async Task ProcessUserAsync(HttpClient httpClient, User user, string url)
+        {
+            try
+            {
+                var json = JsonConvert.SerializeObject(user);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await httpClient.PostAsync(url, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine($"User data for {user.UserGuid} Updated successfully To AR database.\n");
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+                    await PostEvent(jsonResponse);
+                }
+                else
+                {
+                    Console.WriteLine($"Error uploading user data for {user.UserGuid}. Status code: {response.StatusCode}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error processing user data for {user.UserGuid}: {e.Message}");
+            }
+        }
     }
+
+  
 }
